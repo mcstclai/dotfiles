124b2998fa6f8c81b7e3f3f41891a622eba876ec
Matt StClair <matt.stclair@plexus.com>
2018-03-02 14:44:13 -0500

    updated vim build

--- .vim_build.sh
+++ .vim_build.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-~/vim/configure --with-features=huge \
+pushd ~/vim
+make distclean
+./configure \
+	--with-features=huge \
        	--enable-multibyte \
 	--enable-rubyinterp=yes \
-	--enable-pythoninterp=yes \
-	--enable-python3interp=yes \
+	--enable-pythoninterp=dynamic \
+	--with-python-config-dir=/usr/lib64/python2.7/config \
+	--enable-python3interp=dynamic \
+	--with-python3-config-dir=/usr/lib64/python3.4/config-3.4m \
 	--enable-perlinterp=yes \
 	--enable-luainterp=yes \
 	--enable-gui=gtk2 \
 	--enable-cscope \
 	--prefix=/usr
+make VIMRUNTIMEDIR=/home/matt.stclair/vim/runtime VIMDIR=/home/matt.stclair/vim/src
+popd
